79e8e28f9b7889fd879f4943d53cf3c395b89c2c
hongjian <hongjian@youzan.com>
2018-08-05 00:13:13 +0800

    去除datasource依赖限制

--- src/main/java/com/hongjian/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/hongjian/springbootweb/SpringbootWebApplication.java
@@ -3,13 +3,12 @@ package com.hongjian.springbootweb;
 import com.hongjian.springbootweb.config.HongJianDataSourceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * @author hongjian
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @Import({HongJianDataSourceConfiguration.class})
 public class SpringbootWebApplication {
 
